f12ee2a9c072f3186de3f7cd0cad297f87ff721b
Nada Al-Otaibi <82446110+NadaAlOtaibi@users.noreply.github.com>
2021-06-27 16:36:56 +0300

    Update Create-XML-File.cs

--- Create-XML-File.cs
+++ Create-XML-File.cs
@@ -16,25 +16,40 @@ namespace Student
             element1.AppendChild(text1);
 
             XmlElement element2 = xmldoc.CreateElement("to");
-            XmlText text2 = xmldoc.CreateTextNode("Arwa");
+            XmlText text2 = xmldoc.CreateTextNode("Lana");
             root.AppendChild(element2);
             element2.AppendChild(text2);
 
             XmlElement element3 = xmldoc.CreateElement("message");
             XmlText text3 = xmldoc.CreateTextNode("Welcome to c#");
             root.AppendChild(element3);
-            element2.AppendChild(text3);
+            element3.AppendChild(text3);
 
-            xmldoc.Save(@"/Users/nadaalotaibi/Downloads/Decoemnt/email.xml");
+            xmldoc.Save(@"/Users/nadaalotaibi/Downloads/Decomnt/email.xml");
             Console.WriteLine(xmldoc.InnerXml);
 
 
         }
 
+        static void ReadXMLFile()
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(@"/Users/nadaalotaibi/Downloads/Decomnt/email.xml");
+            foreach (XmlNode node in xmldoc.DocumentElement.ChildNodes)
+            {
+                string element = node.Name;
+                Console.Write(element + ": ");
+
+                string text = node.InnerText;
+                Console.WriteLine(text);
+
+            }
+        }
+
         static void Main(string[] args)
         {
             CreateXMLFile();
-
+            ReadXMLFile();
 
         }
             
